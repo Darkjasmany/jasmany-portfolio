ad0c249245e2e55b33b95463b6c57db8a8e730e2
Jasmany Franco - Trabajo <jasmanyfranco@gmail.com>
2026-06-12 16:39:38 -0500

    feat: se corrigio el Hero.astro para que se visualice bien el contenido en pantallas pequeñas

--- src/data/stackData.ts
+++ src/data/stackData.ts
@@ -3,19 +3,19 @@ interface StackData {
 }
 
 export const stackData: StackData[] = [
-  { name: "astro" },
-  { name: "react" },
-  { name: "typeScript" },
-  { name: "tailwindcss" },
-  { name: "nodejs" },
   { name: "HTML5" },
   { name: "CSS3" },
   { name: "javaScript" },
+  { name: "typeScript" },
+  { name: "nodejs" },
+  { name: "react" },
+  { name: "astro" },
+  { name: "tailwindcss" },
   { name: "git" },
-  { name: "supabase" },
   { name: "mysql" },
   { name: "postgresql" },
-  { name: "bash" },
   { name: "docker" },
   { name: "linux" },
+  { name: "bash" },
+  // { name: "supabase" },
 ];
